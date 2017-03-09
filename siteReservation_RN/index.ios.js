37e7338330214c00124fd6945907dae363a46ed5
Nigel Lee <lihui8@le.com>
2017-03-09 18:40:12 +0800

    代码整理，导入图片和标题

--- siteReservation_RN/index.ios.js
+++ siteReservation_RN/index.ios.js
@@ -4,50 +4,48 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
+
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
-export default class siteReservation_RN extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          site reservation, start now!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var MOCKED_MOVIES_DATA = [
+  {title: '标题', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
 });
+
+export default class siteReservation_RN extends Component {
+  render() {
+    var movie = MOCKED_MOVIES_DATA[0];
+    return (
+      <View style={styles.container}>
+
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Image source= {{uri: movie.posters.thumbnail}}
+               style={{width: 150, height: 70}}></Image>
+      </View>
+    );
+  }
+}
 
 AppRegistry.registerComponent('siteReservation_RN', () => siteReservation_RN);
